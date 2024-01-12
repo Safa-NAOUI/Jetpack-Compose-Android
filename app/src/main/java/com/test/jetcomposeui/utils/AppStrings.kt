93c5f35b92e20084f1f5bdf0d2c9c1c7e384e70d
SAFA <safa.naoui96@gmail.com>
2024-01-12 07:47:25 +0100

    add new file appString

---
+++ app/src/main/java/com/test/jetcomposeui/utils/AppStrings.kt
@@ -0,0 +1,11 @@
+package com.test.jetcomposeui.utils
+
+/**
+ * Created by Safa NAOUI on 12/01/2024.
+ */
+object AppStrings {
+
+    const val screenSignInPage = "SignInPage"
+    const val screenSignUpPage = "SignUpPage"
+
+}
